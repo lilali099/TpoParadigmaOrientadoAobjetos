db0572419cbee72e81d12e5f8e63d9826496c1d2
geb2701 <86382687+geb2701@users.noreply.github.com>
2024-06-29 17:49:19 -0300

    cambios dependencias

--- Concesionaria/src/Main.java
+++ Concesionaria/src/Main.java
@@ -1,3 +1,6 @@
+import Classes.Auto;
+import Classes.Moto;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
