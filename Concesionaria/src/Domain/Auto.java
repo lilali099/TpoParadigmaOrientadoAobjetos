4e168fb375495bbb32f8aa7c21ab78c6389bf9a9
Jimena Fernandez <jimenfernandez@uade.edu.ar>
2024-06-30 11:35:35 -0300

    añadir vehiculo

--- Concesionaria/src/Domain/Auto.java
+++ Concesionaria/src/Domain/Auto.java
@@ -32,8 +32,47 @@ public class Auto extends Vehiculo{
         }
         auto.chasis = chasis;
 
+
+
         //completar con todas las propiedades
+
+        Integer cantidadDePuertas = InputFile.obtenerDato("cantidad de puertas:", Integer.class);
+        if (cantidadDePuertas == null) {
+            return null;
+        }
+        auto.cantidadDePuertas = cantidadDePuertas;
+
+        String color = InputFile.obtenerDato("color", String.class);
+        if (color == null) {
+            return null;
+        }
+        auto.setColor(color);;
+
+        String marca  = InputFile.obtenerDato("marca", String.class);
+        if (marca == null) {
+            return null;
+        }
+        auto.setMarca(marca);
+
+        String modelo = InputFile.obtenerDato("modelo", String.class);
+        if (modelo == null) {
+            return null;
+        }
+        auto.setModelo(modelo);
+
+        String patente = InputFile.obtenerDato("patent", String.class);
+        if (patente == null) {
+            return null;
+        }
+        auto.setPatente(patente);
 
         return auto;
     }
+
+
+
+
+
+
+
 }
